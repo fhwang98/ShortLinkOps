5e14fe9587609ee13f290d35a6d879ffd882e616
fhwang98 <fhwang9815@gmail.com>
2026-05-31 12:49:30 +0900

    init

---
+++ src/test/java/com/fhwang/shortlinkops/ShortlinkopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhwang.shortlinkops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
